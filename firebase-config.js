8b0d4a8d2cac9906e2da2b8ee6cce3921663f007
bpoudel816 <bpoudel816@gmail.com>
2026-08-24 15:50:23 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,8 @@
-// DAY 1: Replace ONLY the values below with the Firebase config
-// shown for your Web App in Firebase Console > Project settings > Your apps.
-//
-// IMPORTANT:
-// The normal Firebase Web config is designed to be present in client-side apps.
-// NEVER put service-account JSON, private keys, passwords, or admin credentials here.
-
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_PROJECT_ID_HERE",
-  storageBucket: "PASTE_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_APP_ID_HERE"
+  apiKey: "AIzaSyB855zKmWhD_XVmLevx3j9V3qZtHmrflWg",
+  authDomain: "my-gym-a1da5.firebaseapp.com",
+  projectId: "my-gym-a1da5",
+  storageBucket: "my-gym-a1da5.firebasestorage.app",
+  messagingSenderId: "1065354018613",
+  appId: "1:1065354018613:web:b4615d4d552090b235967c"
 };
